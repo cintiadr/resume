549bd661d5bf54856dc43762af46f3d929b10cb0
cintiadr <cintiadrio@gmail.com>
2017-08-24 23:12:54 +1000

    Update generate-upload-files.sh

--- bin/generate-upload-files.sh
+++ bin/generate-upload-files.sh
@@ -6,7 +6,7 @@ set -x
 
 ./bin/generate-pdf.sh
 
-sftp cintiadr:$SSHPASS@frs.sourceforge.net <<EOF
+sftp cintiadr:$SSHPASS@frs.sourceforge.net -o "StrictHostKeyChecking=no"  <<EOF
   cd userweb/htdocs/resume
   put *.css
   put *.html
