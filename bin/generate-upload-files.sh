06539786a3c6fe9f072fa3dec45d8f1b9c0b8c66
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-09-03 15:14:03 +1000

    Uploading to S3

--- bin/generate-upload-files.sh
+++ bin/generate-upload-files.sh
@@ -5,7 +5,9 @@ set -u
 set -x
 
 mkdir -p target
-
 wget -O target/html2pdf.jar https://s3-ap-southeast-2.amazonaws.com/cintia.me/resume/html2pdf.jar
 
-java -jar target/html2pdf.jar -f resume.html -o target/resume.pdf
+cd resume
+java -jar ../target/html2pdf.jar -f resume.html -o resume.pdf
+
+aws s3 sync . s3://cintia.me/resume
