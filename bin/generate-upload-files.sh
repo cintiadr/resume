102e128e8b1ce260ffff42cdd39e78608afc7c91
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-09-03 15:05:13 +1000

    Attempt to run aws cli

--- bin/generate-upload-files.sh
+++ bin/generate-upload-files.sh
@@ -5,6 +5,7 @@ set -u
 set -x
 
 mkdir -p target
+
 wget -O target/html2pdf.jar https://s3-ap-southeast-2.amazonaws.com/cintia.me/resume/html2pdf.jar
 
 java -jar target/html2pdf.jar -f resume.html -o target/resume.pdf
